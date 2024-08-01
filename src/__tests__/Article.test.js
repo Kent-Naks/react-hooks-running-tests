de6a957e7fdb975273da55ae5dd079950a53aac4
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-08-01 12:45:15 +0300

    Adds

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,22 @@
+// src/__tests__/Article.test.js
+
+// Import libraries needed for testing
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
+// Import the component you wrote
 import Article from "../components/Article";
 
+// Test the component
 test("displays the text 'please pass this test'", () => {
+  // Render the Article component
   render(<Article />);
 
+  // Add the debug line to print the DOM to the console
+  screen.debug();
+
+  // Assertion to check if the text 'please pass this test' is in the document
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
+
